d8c363a0be67b1fdf29ed1007b3f1a99c9ad7d1d
Aasim <a.ahmed77@live.co.uk>
2019-02-12 15:39:47 +0000

    layout fixed

--- src/components/Landing/Mainlanding/Mainlanding.js
+++ src/components/Landing/Mainlanding/Mainlanding.js
@@ -3,11 +3,18 @@ import "./Mainlanding.css";
 
 const Mainlanding = ({title, description, rating, releasedate, background}) => {
     return(
-        <div className="mainlanding_card" style={{backgroundImage: `url(${background})`}}>
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <p>{rating}</p>
-          <p>Release Date : {releasedate}</p>
+        <div className="mainlanding_card" style={{backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${background})`}}>
+            <div>
+
+            </div>
+            <div>
+              <span className="mainlanding_card_title">{title}</span><span> {releasedate} </span>
+              <p> {rating}/10 </p>
+              <p>{description.slice(0, 130) + "..."}</p>
+            </div>
+            <div>
+      
+            </div>
         </div>
 
     )
