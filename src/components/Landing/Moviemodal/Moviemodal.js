5943f0e4bf26c76feda6f219d9865dedb8fd2258
Aasim <a.ahmed77@live.co.uk>
2019-02-21 22:53:14 +0000

    boilerplate movies route

--- src/components/Landing/Moviemodal/Moviemodal.js
+++ src/components/Landing/Moviemodal/Moviemodal.js
@@ -9,14 +9,12 @@ class Moviemodal extends Component {
         currentMovie: ""
     }
 
-    componentDidMount(){
+    async componentDidMount(){
         
-        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=${this.props.api}&language=en-US`)
-        .then(data => data.json())
-        .then(parsed => {
-             this.setState({
-                 currentMovie: {...parsed}
-             })
+        const currentMovie = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=${this.props.api}&language=en-US`)
+        const currentMovieParsed = await currentMovie.json();
+        this.setState({
+            currentMovie: currentMovieParsed
         })
         }
     
@@ -29,19 +27,62 @@ class Moviemodal extends Component {
                 </div>
             )
         }
-        else{
+        console.log(this.state.currentMovie)
+
+        const backgroundImage = this.state.currentMovie.backdrop_path
+        let backgroundURL = `https://image.tmdb.org/t/p/original${backgroundImage}`
+
+
+        const productionCompanies = this.state.currentMovie.production_companies.map(val => {
+            if(val.logo_path !== null){
+               
+            return (
+                <div>
+                <h3>{val.name}</h3>
+                <img width="100px" alt="productioncompany" src={`https://image.tmdb.org/t/p/original${val.logo_path}`}/>
+                </div>
+            )
+            }else{
+                
+            }
+        })
+        
             return(
-        <div>
-        <img src= {`https://image.tmdb.org/t/p/original${this.state.currentMovie.poster_path}`} 
-             alt={this.state.currentMovie.tagline} 
-             width="100px"/>
-        <h2>{this.state.currentMovie.original_title}</h2>
+        <div className="individual_movie_page" style={
+            {background: `url(${backgroundURL})`, 
+            backgroundRepeat:`no-repeat`, 
+            backgroundSize: "cover"}} >
+        
+        <div className="individual_movie_page_pane"> 
+            <a target="_blank" rel="noopener noreferrer" href={this.state.currentMovie.homepage}>
+                <img src= {`https://image.tmdb.org/t/p/original${this.state.currentMovie.poster_path}`} 
+                alt={this.state.currentMovie.tagline} 
+                width="300px"
+             />
+            </a>
+            <h2>{this.state.currentMovie.original_title}</h2>
+
+            <p>{this.state.currentMovie.overview}</p>
+            <a target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${this.state.currentMovie.imdb_id}/?ref_=nv_sr_1`}>
+                <img src={"https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"} alt={"imdb"} width="100px" height="100px"/>
+            </a>
+
+
+            <p><a target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${this.state.currentMovie.imdb_id}/videogallery?ref_=tt_ql_2`}> IMDB Trailers </a></p> 
+            </div>
+            
+            <div className="productionCompanies">
+                {productionCompanies}
+            </div>
+            
+        
+
         </div>
             ) 
     }
     
 }
 
-}
+
 
 export default Moviemodal;
